ad3bf2eeb927432bba908648e1985ee56ee38635
ayuxdev <ayushkr91997@googlemail.com>
2025-11-08 05:33:20 +0530

    Improve domain input handling in email generator
    Refactor email generation script to prompt for domain if not provided as an argument.

--- str_overflow_gen_email.sh
+++ str_overflow_gen_email.sh
@@ -2,6 +2,17 @@
 
 gen_email() {
     local whitelisted_domain="$1"
+    if [ -z $whitelisted_domain ]; then
+        read -p "[+] Enter whitelisted domain: " whitelisted_domain
+    fi
+
+    # Usage
+    if [ -z $whitelisted_domain ]; then
+        echo -e "\nUsage: $0 <whitelisted-domain>"
+        echo "Example: $0 whitelisted.com"
+        exit 1
+    fi
+    
     local prefix_length=$((255 - ${#whitelisted_domain} - 1))
 
     local random_string=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c "$prefix_length")
@@ -10,11 +21,6 @@ gen_email() {
     echo "$final_email"
 }
 
-# Usage
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <whitelisted-domain>"
-    echo "Example: $0 whitelisted.com"
-    exit 1
-fi
+
 
 gen_email "$1"
